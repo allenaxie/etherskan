be22c3aba392119504ea7c1e2c5e5bf21b98f3ce
allenaxie <allenaxie1@gmail.com>
2022-03-15 22:20:20 -0700

    Create layout for latest blocks.

--- components/Blocks/Blocks.tsx
+++ components/Blocks/Blocks.tsx
@@ -1,10 +1,33 @@
 import classes from './Blocks.module.scss';
+import {List} from 'antd';
 
 const Blocks = () => {
+
+    const data = [
+        'Racing car sprays burning fuel into crowd.',
+        'Japanese princess to wed commoner.',
+        'Australian walks 100km after outback crash.',
+        'Man charged over missing wedding girl.',
+        'Los Angeles battles huge wildfires.',
+      ];
+
     return (
-        <>
-            Blocks
-        </>
+        <List
+        className={classes.listContainer}
+        dataSource={data}
+        header={<span className={classes.listHeader}>Latest Blocks</span>}
+        renderItem={item => (
+            <List.Item key="1">
+                <List.Item.Meta
+                title="Title"
+                description="description"
+                />
+                <div>{item}</div>
+                
+            </List.Item>
+        )}
+        />
+            
     )
 }
 
