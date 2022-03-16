ab36811ccf4051df6aca7029ce98249e5937c07e
allenaxie <allenaxie1@gmail.com>
2022-03-15 20:12:18 -0700

    Install npm packages. Create navbar.

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import classes from './Navbar.module.scss';
+import Image from 'next/image';
+
+
+const Navbar = () => {
+    return (
+        <nav className={classes.navContainer}>
+            <div className={classes.logoContainer}>
+                <Image src="/logo.png" width={50} height={50}/>
+                <span>Etherskan</span>
+            </div>
+            <div className={classes.navItemContainer}>
+                <div className={classes.navItem}>
+                    Home
+                </div>
+                <div className={classes.navItem}>
+                    Blockchain
+                </div>
+                <div className={classes.navItem}>
+                    Tokens
+                </div>
+                <div className={classes.navItem}>
+                    Resources
+                </div>
+                <div className={classes.navItem}>
+                    More
+                </div>
+                <div className={classes.navItem}>
+                    Sign In
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
