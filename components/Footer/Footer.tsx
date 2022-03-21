29cd5d092ff9a20d9486e4f42eef7812e1edd413
allenaxie <allenaxie1@gmail.com>
2022-03-20 17:30:52 -0700

    Update footer content and CSS.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import classes from './Footer.module.scss';
+import Image from 'next/image';
 import { Badge, Divider } from 'antd';
 import { GithubOutlined, WechatOutlined, FacebookOutlined, InstagramOutlined } from '@ant-design/icons';
 
@@ -6,8 +7,11 @@ const Footer = () => {
     return (
         <div className={classes.container}>
             <div className={classes.col1}>
-                <span>Etherskan</span>
-                <div>
+                <div className={classes.logoContainer}>
+                    <Image src="/logo.png" width={30} height={30}/>
+                    <span className={classes.logoText}>Etherskan</span>
+                </div>
+                <div className={classes.socialMediaList}>
                     <FacebookOutlined />
                     <InstagramOutlined />
                     <GithubOutlined />
@@ -16,19 +20,39 @@ const Footer = () => {
 
             </div>
             <div className={classes.col2}>
-                <span className={classes.col2Heading}>Company</span>
+                <span className={classes.colHeading}>Company</span>
                 <Divider className={classes.divider}/>
-                <div className={classes.col2List}>
+                <div className={classes.colList}>
                     <span>About Us</span>
                     <span>Contact Us</span>
                     <span>Brand Assets</span>
-                    <span className={classes.careers}>Careers</span>
+                    <span>Careers</span>
                     <span>Terms of Service</span>
                 </div>
-
             </div>
-            <div className={classes.col3}>column 1</div>
-            <div className={classes.col4}>column 1</div>
+            <div className={classes.col3}>
+                <span className={classes.colHeading}>Resources</span>
+                <Divider className={classes.divider}/>
+                <div className={classes.colList}>
+                    <span>API Documentation</span>
+                    <span>Knowledge Base</span>
+                    <span>Newsletter</span>
+                    <span>Network Status</span>
+                    <span>Disqus Comments</span>
+                </div>
+            </div>
+            <div className={classes.col4}>
+                <span className={classes.colHeading}>Products & Services</span>
+                <Divider className={classes.divider}/>
+                <div className={classes.colList}>
+                    <span>Advertise</span>
+                    <span>Explorer-as-a-Service (EaaS)</span>
+                    <span>Developer APIs</span>
+                    <span>Blockscan</span>
+                    <span>BeaconScan</span>
+                </div>
+            </div>
+            
         </div>
     )
 }
