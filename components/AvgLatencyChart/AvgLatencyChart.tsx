92bf9dcd050dcf33c9f24a473a35c8dadfa21400
allenaxie <allenaxie1@gmail.com>
2022-03-21 20:33:54 -0700

    Display latency chart. Format charts layout.

--- components/AvgLatencyChart/AvgLatencyChart.tsx
+++ components/AvgLatencyChart/AvgLatencyChart.tsx
@@ -1,5 +1,6 @@
 import classes from './AvgLatencyChart.module.scss';
 import { Line } from 'react-chartjs-2';
+import {useState} from 'react';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -25,42 +26,54 @@ ChartJS.register(
 
 const AvgLatencyChart = () => {
 
-    const latencyData:any = [
+    const LatencyData:any = [
         {
             date: '03/15',
-            value: 1.098,
+            value: 13.10,
           },
           {
             date: '03/16',
-            value: 1.190,
+            value: 13.25,
           },
           {
             date: '03/17',
-            value: 1.236,
+            value: 13.27,
           },
           {
             date: '03/18',
-            value: 1.175,
+            value: 13.24,
           },
           {
             date: '03/19',
-            value: 1.266,
+            value: 13.19,
           },
           {
             date: '03/20',
-            value: 1.128,
+            value: 13.17,
           },
           {
             date: '03/21',
-            value: 1.119,
+            value: 13.19,
           },
     ]
+
+    const [latencyData, setLatencyData] = useState({
+        labels: LatencyData.map((data) => data.date),
+        datasets:[{
+          label: "Ethereum Average Latency (seconds)",
+          data: LatencyData.map((data) => data.value),
+          backgroundColor: ['rgb(250, 193, 71)'],
+          borderColor: "black",
+          borderWidth:1,
+        }]
+      })
+
     return (
         <div className={classes.container}>
             <div style={{ width: 300 }}>
-                {/* <Line
+                <Line
                     data={latencyData}
-                /> */}
+                />
             </div>
         </div>
     )
